3ca5a833af1f80b58ae5535c5b029731cbca323e
Madmegsox <madmegsox2@gmail.com>
2023-08-13 21:07:40 +0100

    [Addition][Tests] Added some event bus tests

---
+++ src/test/java/org/polyscape/test/auto/EventBusTest.java
@@ -0,0 +1,47 @@
+package org.polyscape.test.auto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.polyscape.event.Event;
+import org.polyscape.event.EventBus;
+import org.polyscape.event.IEvent;
+
+/**
+ * @author Madmegsox1
+ * @since 13/08/2023
+ */
+
+public class EventBusTest {
+
+    @Test
+    public void initTest(){
+        EventBus bus = new EventBus();
+        Assertions.assertNotNull(bus);
+    }
+
+    @Test
+    public void eventPushTest(){
+        EventBus bus = new EventBus();
+
+        IEvent<TestEvent> event = e -> {
+            Assertions.assertEquals(e.result, 1);
+        };
+
+
+        TestEvent.addEvent(event, TestEvent.class);
+
+        bus.postEvent(new TestEvent(1));
+    }
+
+
+
+    static class TestEvent extends Event<TestEvent>{
+
+        public final int result;
+
+        public TestEvent(int i){
+            this.result = i;
+        }
+    }
+
+}
